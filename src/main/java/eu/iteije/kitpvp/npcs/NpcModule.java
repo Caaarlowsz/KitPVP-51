dc61c1d08f4980374f6f0b09950495d192050dcd
Teije <iteije@outlook.com>
2021-03-22 19:05:47 +0100

    Texture the NPC to be black

--- src/main/java/eu/iteije/kitpvp/npcs/NpcModule.java
+++ src/main/java/eu/iteije/kitpvp/npcs/NpcModule.java
@@ -7,6 +7,7 @@ import eu.iteije.kitpvp.pluginutils.Message;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.event.SpawnReason;
 import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.trait.SkinTrait;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -57,7 +58,7 @@ public class NpcModule {
         if (this.joinNpcs.containsKey(map)) delete(map);
 
         NPC npc = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, "&dClick to join");
-//        npc.data().setPersistent(NPC.);
+        this.setSkin(npc);
 
         npc.spawn(location, SpawnReason.PLUGIN);
 
@@ -102,15 +103,16 @@ public class NpcModule {
     }
 
     public void unload() {
-//        for (NPC npc : this.joinNpcs.values()) {
-//            npc.destroy();
-//            CitizensAPI.getNPCRegistry().deregisterAll();
-//        }
         CitizensAPI.getNPCRegistry().deregisterAll();
     }
 
-//    private NPC setSkin(NPC npc) {
-//
-//    }
+    private NPC setSkin(NPC npc) {
+        npc.getOrAddTrait(SkinTrait.class).setSkinPersistent(
+                "639601793",
+                "x7fGrW9QImG5fIVOpPiMqXCAEJKqUFck/yAqkY+xCyaFIVHJz9K/q+3vf/ESVfTbGoVU9yt2d1FKAb3OrV0G/tfmz55TFa3TDQdBGweGiViaexPNwPHKHxlychvSqD6DKWofuUOdhJIwzx+uraGU4310oZ6JeKxWuAuBJZtSDBBn+Vlfriqsh7CrZ43bupDENu67iMTuuJI/e2NoKYUumG16TYJI4CdtiRPQHM4kQhDQWma/CjCxzdaLTeZg14uJoR3TUEmsD35yzyhm9iU4eHia3Oo7YTYVnGZTlkJqYODfhUZB6D+FspV9RuLEAawQB6hRKcKoQvTdEQF05FzyZwY72lhofeAKNnhmQQXgxQWWZpJqvSgAP4webO6btCaTdRGmkuuteIP85abUjHUMMxz6uYiEWoeh3FvQ9IChz9UZPRf4i6wiPMOuedJAbnL+n3wXYr+7+6lbfqhK6Q6YRe4MC8ouwb4SSUJ8KNuBhkCK7438up2fA+yMVwABcckJ6yVHoyAneAmPJpUvT76qzZsPDYv9QVtrUOpquS26eDlF1pVI4S5LbdsakYRJFr1CwMduGdGeY1xYnAdM+MBwBzHjQdYfsNJHUWt1ze28UUTOZrZVAcpXoVFD4bfL5gi35Ob/sBUECgzfM/p2ZH5h2ulby7dX3c9F3vNDVkGyDP8=",
+                "ewogICJ0aW1lc3RhbXAiIDogMTYxMTI4NjE1MTk1NywKICAicHJvZmlsZUlkIiA6ICJiMGQ3MzJmZTAwZjc0MDdlOWU3Zjc0NjMwMWNkOThjYSIsCiAgInByb2ZpbGVOYW1lIiA6ICJPUHBscyIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS81NjRlMWEzODg5YzFiNDI5NzgwNDE2MDY0NTY4NGE1NGE2ZGNlY2VmMzc1Nzc5OWRlYTVmMzc3NDNhMWI1MjY0IiwKICAgICAgIm1ldGFkYXRhIiA6IHsKICAgICAgICAibW9kZWwiIDogInNsaW0iCiAgICAgIH0KICAgIH0KICB9Cn0="
+        );
+        return npc;
+    }
 
 }
